7f08e7d55be625fdb999885c92d9a8955386e701
cezarszl <cezarszlfedora@gmail.com>
2023-11-23 04:07:17 +0100

    Added example of using SQL db with Sequelize
    Bonus Task Exercise 2.8

--- index_sql.js
+++ index_sql.js
@@ -1,11 +1,37 @@
 const express = require('express')
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const bodyParser = require('body-parser');
 const Models = require('./models_sql');
 
 const app = express();
 
-const users = Models.User;
+
+
+const sequelize = new Sequelize({
+    database: "mydb",
+    username: "postgres",
+    password: "elo350",
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres",
+    define: {
+        timestamps: false
+    }
+
+});
+
+const testDbConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
+
+testDbConnection();
+
+const users = Models.User(sequelize, DataTypes);
 
 app.use(bodyParser.json());
 
--- models_sql.js
+++ models_sql.js
@@ -1,49 +1,27 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
+const userModel = function (sequelize, DataTypes) {
 
-const sequelize = new Sequelize({
-    database: "mydb",
-    username: "postgres",
-    password: "elo350",
-    host: "127.0.0.1",
-    port: 5432,
-    dialect: "postgres",
-    define: {
-        timestamps: false
-    }
+    return sequelize.define('user', {
 
-});
-
-const testDbConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
-    } catch (error) {
-        console.error("Unable to connect to the database:", error);
-    }
+        userid: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.STRING
+        },
+        email: {
+            type: DataTypes.STRING
+        },
+        birth_date: {
+            type: DataTypes.DATE
+        }
+    })
 };
-
-testDbConnection();
-
-const userModel = sequelize.define('user', {
-
-    userid: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true
-    },
-    username: {
-        type: DataTypes.STRING
-    },
-    password: {
-        type: DataTypes.STRING
-    },
-    email: {
-        type: DataTypes.STRING
-    },
-    birth_date: {
-        type: DataTypes.DATE
-    }
-});
 
 module.exports.User = userModel;
