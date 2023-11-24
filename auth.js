991fb7cefddbe2cb88e76e506f9d11a7ddd6b77a
cezarszl <cezarszlfedora@gmail.com>
2023-11-24 05:37:33 +0100

    Created separated file with login endpoint and auth logic
    Full-Stack Immersion Exercise 2.9 Task

---
+++ auth.js
@@ -0,0 +1,32 @@
+const jwtSecret = 'secret';
+
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+require('./passport');
+
+let issueJWT = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username,
+        expiresIn: '2d',
+        algorithm: 'HS256'
+    });
+}
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false }, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is wrong',
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = issueJWT(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+}
