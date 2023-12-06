3985fcebe458a08fa509055605435b3abf07c14d
cezarszl <cezarszlfedora@gmail.com>
2023-12-06 20:46:41 +0100

    Added localhost:1234 to CORS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express'),
 
 const app = express();
 require('dotenv').config();
-let allowedOrigins = ['http://localhost:8080', 'https://cezarszlmyflix-0212aa467a8d.herokuapp.com/'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://cezarszlmyflix-0212aa467a8d.herokuapp.com/'];
 
 app.use(cors({
   origin: (origin, callback) => {
